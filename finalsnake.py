47f37c21930489cba236671e83ae83e7a4e02bec
MelMuel <54798899+MelMuel@users.noreply.github.com>
2019-09-03 18:34:38 +0200

    Update finalsnake.py

--- finalsnake.py
+++ finalsnake.py
@@ -1,6 +1,7 @@
 #Snake Tutorial Python
 import RPi.GPIO as gpio
-
+import time
+import Adafruit_CharLCD as LCD
 import math
 import random
 import pygame
@@ -20,6 +21,19 @@ gpio.setup(button3, gpio.IN)
 gpio.setup(button4, gpio.IN)
 gpio.setup (button5, gpio.IN)
 gpio.setup(button6, gpio.IN)
+
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 2
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
 
 WHITE_COLOR = (255, 255, 255) # Color for score display
 pygame.font.init()
